f8fd3d366d423b3de763b9102adf58ad70ca1ee3
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-24 22:33:59 +0530

    Added: Calculate Wage till the condition is set

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,16 +1,32 @@
-//UC3 
-function getWorkingHour(empCheck) {
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 100;
+
+function getWorkinghours(empCheck) {
     switch (empCheck) {
-        case IS | _PART_TIME:
-            return _PART_TIME_HOURS;
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
         case IS_FULL_TIME:
             return FULL_TIME_HOURS;
         default:
             return 0;
     }
 }
-let empHrs =0;
-let empCheck = Math.floor((Math.random() *10)% 3);
-empHrs = getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOURS;
-console.log("EMP Wage" + empCheckmpWage);
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkinghours(empCheck);
+}
+
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+
+console.log("Total Working days: " + totalWorkingDays);
+console.log("Total Employee Hours= " + totalEmpHrs);
+console.log("Total Employee Wage =" + empWage);
